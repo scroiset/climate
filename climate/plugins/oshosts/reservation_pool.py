67b66e2559fcf87672eb4f5f343ae12ecf2e134d
Swann Croiset <swann.croiset@bull.net>
2013-11-13 14:26:48 +0100

    Nova Aggregate wrapper to manage host allocation pools
    Change-Id: Ife4949896c1679c941df5b7059a8fbcbf55ec287

---
+++ climate/plugins/oshosts/reservation_pool.py
@@ -0,0 +1,324 @@
+# Copyright (c) 2013 OpenStack Foundation
+#
+# Author: Swann Croiset <swann.croiset@bull.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid as uuidgen
+
+from novaclient import client
+from novaclient import exceptions as nova_exceptions
+from oslo.config import cfg
+
+from climate import context
+from climate.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+opts = [
+    cfg.StrOpt('aggregate_freepool_name',
+               default='freepool',
+               help='Name of the special aggregate where all hosts '
+                    'are candidate for physical host reservation'),
+]
+
+cfg.CONF.register_opts(opts, 'physical:host')
+
+TENANT_ID_KEY = 'climate:tenant'
+CLIMATE_OWNER = 'climate:owner'
+CLIMATE_AZ_PREFIX = 'climate:'
+
+
+class NoFreePool(Exception):
+    pass
+
+
+class HostNotInFreePool(Exception):
+    msg = "Host %s not in freepool '%s'"
+
+    def __init__(self, host, freepool_name):
+        self.host = host
+        self.freepool = freepool_name
+
+    def __str__(self):
+        return self.msg % (self.host,
+                           self.freepool)
+
+
+class CantRemoveHost(Exception):
+    pass
+
+
+class AggregateHaveHost(Exception):
+    msg = "Can't delete Aggregate '%s', host(s) attached to it : %s"
+
+    def __init__(self, pool):
+        self.pool = pool
+
+    def __str__(self):
+        return self.msg % (self.pool.name,
+                           ','.join(self.pool.hosts))
+
+
+class AggregateNotFound(Exception):
+    msg = "Aggregate '%s' not found!"
+
+    def __init__(self, pool):
+        self.pool = pool
+
+    def __str__(self):
+        return self.msg % (self.pool)
+
+
+class ReservationPool(object):
+    def __init__(self):
+        self.ctx = context.current()
+        self.freepool_name = cfg.CONF['physical:host'].aggregate_freepool_name
+
+        #TODO(scroiset): use catalog to find the url
+        auth_url = "%s://%s:%s/v2.0" % (cfg.CONF.os_auth_protocol,
+                                        cfg.CONF.os_auth_host,
+                                        cfg.CONF.os_auth_port
+                                        )
+        self.nova = client.Client('2',
+                                  username=cfg.CONF.os_admin_username,
+                                  api_key=cfg.CONF.os_admin_password,
+                                  auth_url=auth_url,
+                                  project_id=cfg.CONF.os_admin_tenant_name
+                                  )
+
+    def get_aggregate_from_name(self, name):
+
+        #FIXME(scrosiet): can't get an aggregate by name
+        # so iter over all aggregate and check for the good one!
+        all_aggregates = self.nova.aggregates.list()
+        for agg in all_aggregates:
+            if name == agg.name:
+                return agg
+        else:
+            return None
+
+    def get_aggregate_from_id(self, aggregate_or_id):
+        """id can be an aggregate or an id."""
+        agg_id = None
+        try:
+            agg_id = int(aggregate_or_id)
+        except (ValueError, TypeError):
+            if hasattr(aggregate_or_id, 'id') and aggregate_or_id.id:
+                # pool is an aggregate
+                agg_id = aggregate_or_id.id
+
+        if agg_id is not None:
+            return self.nova.aggregates.get(agg_id)
+
+    def get_aggregate_from_name_or_id(self, whatever):
+        """Return an aggregate by name or an id."""
+
+        aggregate = self.get_aggregate_from_id(whatever)
+        if aggregate is None:
+            return self.get_aggregate_from_name(whatever)
+        return aggregate
+
+    @staticmethod
+    def _generate_aggregate_name():
+        return str(uuidgen.uuid4())
+
+    def create(self, az=True):
+        """Create a Pool (an Aggregate) with or without Availability Zone.
+
+        By default expose to user the aggregate with an Availability Zone.
+        Return an aggregate or raise a nova exception.
+
+        """
+
+        name = self._generate_aggregate_name()
+
+        az_name = None
+        if az:
+            az_name = "%s%s" % (CLIMATE_AZ_PREFIX, name)
+
+        if az_name is None:
+            LOG.debug('Creating pool aggregate: %s'
+                      'without Availability Zone' % name
+                      )
+            agg = self.nova.aggregates.create(name)
+        else:
+            LOG.debug('Creating pool aggregate: %s'
+                      'with Availability Zone %s' % (name, az_name)
+                      )
+            agg = self.nova.aggregates.create(name, az_name)
+
+        meta = {CLIMATE_OWNER: self.ctx.tenant_id}
+        self.nova.aggregates.set_metadata(agg, meta)
+
+        return agg
+
+    def delete(self, pool, force=True):
+        """Delete an aggregate.
+
+        pool can be an aggregate name or an aggregate id.
+        Remove all hosts before delete aggregate (default).
+        If force is False, raise exception if at least one
+        host is attached to.
+
+        """
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+
+        if agg is None:
+            LOG.warning("No aggregate associate with name or id %s" % pool)
+            return
+
+        hosts = agg.hosts
+        if len(hosts) > 0 and not force:
+            raise AggregateHaveHost(agg)
+
+        for host in hosts:
+            LOG.debug("Removing host '%s' from aggregate "
+                      "'%s')" % (host, agg.id))
+            self.nova.aggregates.remove_host(agg.id, host)
+            self.nova.aggregates.add_host(self.freepool_name, host)
+
+        self.nova.aggregates.delete(agg.id)
+
+    def get_all(self):
+        """Return all aggregate."""
+
+        return self.nova.aggregates.list()
+
+    def get(self, pool):
+        """return details for aggregate pool or None."""
+
+        return self.get_aggregate_from_name_or_id(pool)
+
+    def get_computehosts(self, pool):
+        """Return a list of compute host names for an aggregate."""
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+
+        if agg:
+            return agg.hosts
+        return []
+
+    def add_computehost(self, pool, host):
+        """Add a compute host to an aggregate.
+
+        The `host` must exist otherwise raise an error
+        and the `host` must be in the freepool.
+
+        Return the related aggregate.
+        Raise an aggregate exception if something wrong.
+        """
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+
+        if agg is None or agg.id is None:
+            LOG.warning("Can't add a computehost "
+                        "to an inexistant Aggregate '%s' " % agg)
+            return
+
+        freepool_agg = self.get(self.freepool_name)
+        if freepool_agg is None:
+            raise NoFreePool()
+
+        if host not in freepool_agg.hosts:
+            raise HostNotInFreePool(host, freepool_agg.name)
+
+        LOG.info("removing host '%s' "
+                 "to aggregate freepool %s" % (host,
+                                               freepool_agg.name))
+        self.remove_computehost(freepool_agg.id, host)
+
+        LOG.info("adding host '%s' to aggregate %s" % (host, agg.id))
+        try:
+            return self.nova.aggregates.add_host(agg.id, host)
+        except nova_exceptions.ClientException as e:
+            LOG.exception(e)
+
+    def add_computehost_to_freepool(self, host):
+        freepool_agg = self.get(self.freepool_name)
+        if freepool_agg is None:
+            raise NoFreePool()
+
+        if host in freepool_agg.hosts:
+            LOG.warning('Host is already in the freepool')
+            return
+
+        return self.nova.aggregates.add_host(freepool_agg.id, host)
+
+    def remove_all_computehosts(self, pool):
+        """Remove all compute hosts attached to an aggregate."""
+
+        hosts = self.get_computehosts(pool)
+        self.remove_computehost(pool, hosts)
+
+    def remove_computehost(self, pool, hosts):
+        """Remove compute host(s) from an aggregate."""
+
+        if not isinstance(hosts, list):
+            hosts = [hosts]
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+        if agg is None:
+            raise AggregateNotFound(pool)
+
+        freepool_agg = self.get(self.freepool_name)
+        if freepool_agg is None:
+            raise NoFreePool()
+
+        for host in hosts:
+            try:
+                self.nova.aggregates.remove_host(agg.id, host)
+                self.nova.aggregates.add_host(freepool_agg.id, host)
+            except nova_exceptions.ClientException as e:
+                raise CantRemoveHost(e)
+
+    def remove_computehost_from_freepool(self, hosts):
+
+        if not isinstance(hosts, list):
+            hosts = [hosts]
+
+        freepool_agg = self.get(self.freepool_name)
+        if freepool_agg is None:
+            raise NoFreePool()
+
+        for host in hosts:
+            if host not in freepool_agg.hosts:
+                raise HostNotInFreePool(host, freepool_agg.name)
+
+            try:
+                self.nova.aggregates.remove_host(freepool_agg.id, host)
+            except nova_exceptions.ClientException as e:
+                raise CantRemoveHost(e)
+
+    def add_project(self, pool, project_id):
+        """Add a project to an aggregate."""
+
+        metadata = {project_id: TENANT_ID_KEY}
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+        if agg is None:
+            raise nova_exceptions.NotFound("No aggregate '%s'" % pool)
+
+        return self.nova.aggregates.set_metadata(agg.id, metadata)
+
+    def remove_project(self, pool, project_id):
+        """Remove a project from an aggregate."""
+
+        agg = self.get_aggregate_from_name_or_id(pool)
+        if agg is None:
+            raise nova_exceptions.NotFound("Can't add project to an"
+                                           "nonexistent aggregate %s" % pool)
+        metadata = {project_id: None}
+        return self.nova.aggregates.set_metadata(agg.id, metadata)
---
+++ climate/tests/test_reservation_pool.py
@@ -0,0 +1,248 @@
+# Copyright (c) 2013 Openstack Fondation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from oslo.config import cfg
+
+from climate import config
+from climate import context
+from climate import tests
+from climate.plugins.oshosts import reservation_pool as rp
+
+
+class AggregateFake(object):
+
+    def __init__(self, i, name, hosts):
+        self.id = i
+        self.name = name
+        self.hosts = hosts
+
+
+class ReservationPoolTestCase(tests.TestCase):
+
+    def setUp(self):
+        super(ReservationPoolTestCase, self).setUp()
+        config.parse_configs()
+        self.pool_name = 'pool-name-xxx'
+        self.tenant_id = 'tenant-uuid'
+        self.fake_aggregate = AggregateFake(i=123,
+                                            name='fooname',
+                                            hosts=['host1', 'host2']
+                                            )
+        self.freepool_name = cfg.CONF['physical:host'].aggregate_freepool_name
+        self.fake_freepool = AggregateFake(i=456,
+                                           name=self.freepool_name,
+                                           hosts=['host3']
+                                           )
+
+        self.set_context(context.ClimateContext(tenant_id=self.tenant_id))
+
+        self.nova = mock.MagicMock()
+        mock.patch('novaclient.client.Client',
+                   return_value=self.nova).start()
+
+        self.nova.aggregates.set_metadata = mock.MagicMock()
+        self.nova.aggregates.remove_host = mock.MagicMock()
+
+        self.pool = rp.ReservationPool()
+
+        self.p_name = self.patch(self.pool, '_generate_aggregate_name')
+        self.p_name.return_value = self.pool_name
+
+    def _patch_get_agg_from_whatever(self):
+        def get_fake_aggregate(*args):
+            if self.freepool_name in args:
+                return self.fake_freepool
+            else:
+                return self.fake_aggregate
+
+        self.patch(self.pool, 'get_aggregate_from_name_or_id')\
+            .side_effect = get_fake_aggregate
+
+    def test_get_agg_from_id(self):
+        self.pool.get_aggregate_from_id(123)
+        self.nova.aggregates.get\
+                 .assert_called_once_with(123)
+
+        self.nova.aggregates.get.reset_mock()
+        self.pool.get_aggregate_from_id("456")
+        self.nova.aggregates.get\
+                 .assert_called_once_with(456)
+
+        self.nova.aggregates.get.reset_mock()
+        self.pool.get_aggregate_from_id(self.fake_aggregate)
+        self.nova.aggregates.get\
+                 .assert_called_once_with(self.fake_aggregate.id)
+
+    def test_get_agg_from_name(self):
+        self.nova.aggregates.list.return_value = [self.fake_aggregate]
+
+        self.assertEqual(self.pool.get_aggregate_from_name('none'),
+                         None)
+        self.assertEqual(self.pool.get_aggregate_from_name('fooname'),
+                         self.fake_aggregate)
+
+    def test_get_agg_from_whatever(self):
+        self.nova.aggregates.list.return_value = [self.fake_aggregate]
+
+        self.assertEqual(self.pool.get_aggregate_from_name_or_id('none'), None)
+        self.assertEqual(self.pool.get_aggregate_from_name_or_id('fooname'),
+                         self.fake_aggregate)
+
+        self.nova.aggregates.get.reset_mock()
+        self.pool.get_aggregate_from_name_or_id(123)
+        self.nova.aggregates.get\
+                 .assert_called_once_with(123)
+
+        self.nova.aggregates.get.reset_mock()
+        self.pool.get_aggregate_from_name_or_id("456")
+        self.nova.aggregates.get\
+                 .assert_called_once_with(456)
+
+        self.nova.aggregates.get.reset_mock()
+        self.pool.get_aggregate_from_name_or_id(self.fake_aggregate)
+        self.nova.aggregates.get\
+                 .assert_called_once_with(self.fake_aggregate.id)
+
+    def test_create(self):
+        self.nova.aggregates.create = mock.MagicMock(
+            return_value=self.fake_aggregate
+        )
+
+        agg = self.pool.create()
+
+        self.assertEqual(agg, self.fake_aggregate)
+
+        az_name = rp.CLIMATE_AZ_PREFIX + self.pool_name
+        self.nova.aggregates.create\
+                            .assert_called_once_with(self.pool_name,
+                                                     az_name)
+
+        meta = {rp.CLIMATE_OWNER: self.tenant_id}
+        self.nova.aggregates.set_metadata\
+                            .assert_called_once_with(self.fake_aggregate, meta)
+
+    def test_create_no_az(self):
+        self.nova.aggregates.create = mock.MagicMock(
+            return_value=self.fake_aggregate
+        )
+
+        self.pool.create(az=False)
+
+        self.nova.aggregates.create.assert_called_once_with(self.pool_name)
+
+    def test_delete_with_host(self):
+        self._patch_get_agg_from_whatever()
+        agg = self.pool.get('foo')
+
+        self.pool.delete(agg)
+        self.nova.aggregates.delete.assert_called_once_with(agg.id)
+        for h in agg.hosts:
+            self.nova.aggregates.remove_host.assert_any_call(agg.id, h)
+            self.nova.aggregates.add_host.assert_any_call(
+                self.fake_freepool.name,
+                h
+            )
+
+        # can't delete aggregate with hosts
+        self.assertRaises(rp.AggregateHaveHost,
+                          self.pool.delete, 'bar',
+                          force=False)
+
+    def test_delete_with_no_host(self):
+        self._patch_get_agg_from_whatever()
+        agg = self.pool.get('foo')
+        agg.hosts = []
+        self.pool.delete('foo', force=False)
+        self.nova.aggregates.delete.assert_called_once_with(agg.id)
+
+    def test_get_all(self):
+        self.pool.get_all()
+        self.nova.aggregates.list.assert_called_once_with()
+
+    def test_get(self):
+        self._patch_get_agg_from_whatever()
+        agg = self.pool.get('foo')
+        self.assertEqual(self.fake_aggregate,
+                         agg)
+
+    def test_add_computehost(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.add_computehost('pool', 'host3')
+
+        self.nova.aggregates.add_host\
+                            .assert_any_call(self.fake_aggregate.id,
+                                             'host3')
+        self.nova.aggregates.remove_host\
+                            .assert_any_call(self.fake_aggregate.id,
+                                             'host3')
+
+    def test_add_computehost_not_in_freepool(self):
+        self._patch_get_agg_from_whatever()
+        self.assertRaises(rp.HostNotInFreePool,
+                          self.pool.add_computehost,
+                          'foopool',
+                          'ghost-host')
+
+    def test_add_computehost_to_freepool(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.add_computehost_to_freepool('host2')
+        self.nova.aggregates.add_host\
+                            .assert_called_once_with(self.fake_freepool.id,
+                                                     'host2')
+
+    def test_remove_computehost_from_freepool(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.remove_computehost_from_freepool('host3')
+
+        self.nova.aggregates.remove_host\
+                            .assert_called_once_with(self.fake_freepool.id,
+                                                     'host3')
+
+    def test_remove_computehost_from_freepool_not_in(self):
+        self._patch_get_agg_from_whatever()
+        self.assertRaises(rp.HostNotInFreePool,
+                          self.pool.remove_computehost_from_freepool,
+                          'hostXX')
+
+    def test_remove_allcomputehosts(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.remove_all_computehosts('pool')
+        for h in self.fake_aggregate.hosts:
+            self.nova.aggregates.remove_host\
+                                .assert_any_call(self.fake_aggregate.id,
+                                                 h)
+
+    def test_get_computehosts(self):
+        self._patch_get_agg_from_whatever()
+        hosts = self.pool.get_computehosts('foo')
+        self.assertEqual(hosts, self.fake_aggregate.hosts)
+
+    def test_add_project(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.add_project('pool', 'projectX')
+        self.nova.aggregates.set_metadata\
+                 .assert_called_once_with(self.fake_aggregate.id,
+                                          {'projectX': rp.TENANT_ID_KEY}
+                                          )
+
+    def test_remove_project(self):
+        self._patch_get_agg_from_whatever()
+        self.pool.remove_project('pool', 'projectY')
+        self.nova.aggregates.set_metadata\
+                 .assert_called_once_with(self.fake_aggregate.id,
+                                          {'projectY': None}
+                                          )
